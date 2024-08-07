fa3f632ae64cfc8edca343bc3c75f100ef29692e
vaibdix <dixitvaib@gmail.com>
2024-08-08 00:12:07 +0530

    feat: create question collection

---
+++ src/models/server/question.collection.ts
@@ -0,0 +1,47 @@
+import { IndexType, Permission } from "node-appwrite"
+
+import { db, questionCollection } from "../name"
+import { databases } from "./config"
+
+export default async function createQuestionCollection() {
+  // collection creation
+  await databases.createCollection(db, questionCollection, questionCollection, [
+    Permission.read("any"),
+    Permission.read("users"),
+    Permission.create("users"),
+    Permission.update("users"),
+    Permission.delete("users"),
+  ])
+  console.log("question_Collection is created")
+
+  // creating indexes and attributes
+
+  await Promise.all([
+    databases.createStringAttribute(db,questionCollection, "title", 100, true),
+    databases.createStringAttribute(db,questionCollection, "content", 10000, true),
+    databases.createStringAttribute(db,questionCollection, "authorId", 10000, true),
+    databases.createStringAttribute(db,questionCollection, "tags", 50, true, undefined, true),
+    databases.createStringAttribute(db,questionCollection, "attachmentId", 50, false),
+  ]);
+  console.log("Question attributes creation")
+
+
+  // await Promise.all([
+  //   databases.createIndex(
+  //     db,
+  //     questionCollection,
+  //     "title",
+  //     IndexType.Fulltext,
+  //     ["title"],
+  //     ['asc']
+  //   ),
+  //   databases.createIndex(
+  //     db,
+  //     questionCollection,
+  //     "content",
+  //     IndexType.Fulltext,
+  //     ["content"],
+  //     ['asc']
+  //   )
+  // ])
+}
