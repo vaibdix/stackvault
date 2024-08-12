b6ec9fc39b759636a6b710a26fb7691e74f01a61
vaibdix <dixitvaib@gmail.com>
2024-08-12 23:20:16 +0530

    chore: zustand store

--- src/store/Auth.ts
+++ src/store/Auth.ts
@@ -38,3 +38,87 @@ interface IAuthStore {
   logout(): Promise<void>
 }
 
+
+export const useAuthStore = create<IAuthStore>()(
+  persist(
+    immer((set) => ({
+      session: null,
+      jwt: null,
+      user: null,
+      hydrated: false,
+
+      setHydrated() {
+        set({hydrated: true})
+      },
+
+      async verfiySession() {
+        try {
+          const session = await account.getSession("current")
+          set({session})
+
+        } catch (error) {
+          console.log(error)
+        }
+      },
+
+      async login(email: string, password: string) {
+        try {
+          const session = await account.createEmailPasswordSession(email, password)
+          const [user, {jwt}] = await Promise.all([
+            account.get<UserPrefs>(),
+            account.createJWT()
+
+          ])
+          if (!user.prefs?.reputation) await account.updatePrefs<UserPrefs>({
+            reputation: 0
+          })
+
+          set({session, user, jwt})
+
+          return { success: true}
+
+        } catch (error) {
+
+          console.log(error)
+          return {
+            success: false,
+            error: error instanceof AppwriteException ? error: null,
+
+          }
+        }
+      },
+
+      async createAccount(name:string, email: string, password: string) {
+        try {
+          await account.create(ID.unique(), email, password, name)
+          return {success: true}
+        } catch (error) {
+          console.log(error)
+          return {
+            success: false,
+            error: error instanceof AppwriteException ? error: null,
+
+          }
+        }
+      },
+
+      async logout() {
+        try {
+          await account.deleteSessions()
+          set({session: null, jwt: null, user: null})
+
+        } catch (error) {
+          console.log(error)
+        }
+      },
+    })),
+    {
+      name: "auth",
+      onRehydrateStorage(){
+        return (state, error) => {
+          if (!error) state?.setHydrated()
+        }
+      }
+    }
+  )
+)
